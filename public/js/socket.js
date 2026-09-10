d903d9e5b9cd536afa345f35f7ef9b8ad1901158
Kleber Bispo <102396294+klebomtv@users.noreply.github.com>
2026-09-09 21:11:18 -0300

    Update socket.js

--- public/js/socket.js
+++ public/js/socket.js
@@ -4,3 +4,33 @@ export const socket =
     io({
         autoConnect: false
     })
+
+socket.on(
+    'connect',
+    () => {
+        console.log(
+            '[SOCKET CLIENT] Conectado:',
+            socket.id
+        )
+    }
+)
+
+socket.on(
+    'connect_error',
+    error => {
+        console.error(
+            '[SOCKET CLIENT] Erro de conexão:',
+            error.message
+        )
+    }
+)
+
+socket.on(
+    'disconnect',
+    reason => {
+        console.warn(
+            '[SOCKET CLIENT] Desconectado:',
+            reason
+        )
+    }
+)
