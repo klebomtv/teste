afa0b95a329b037fc3c43baf2e44c7205cc13113
Kleber Bispo <102396294+klebomtv@users.noreply.github.com>
2026-09-14 19:51:09 -0300

    Update groups.js

--- src/whatsapp/groups.js
+++ src/whatsapp/groups.js
@@ -1,21 +1,31 @@
-
 // caminho: src/whatsapp/groups.js
 
-async function loadGroups(
-    sock
-) {
+async function loadGroups(sock) {
 
     if (!sock) {
-
         throw new Error(
             'Socket do WhatsApp não disponível.'
         )
-
     }
 
+    console.log(
+        '[GROUPS] Buscando grupos no WhatsApp...'
+    )
 
     const participating =
         await sock.groupFetchAllParticipating()
+
+    console.log(
+        '[GROUPS] JSON recebido do WhatsApp:'
+    )
+
+    console.dir(
+        participating,
+        {
+            depth: null,
+            colors: false
+        }
+    )
 
 
     const groups =
@@ -34,22 +44,21 @@ async function loadGroups(
 
             })
         )
-        .sort(
-            (a, b) =>
-                a.name.localeCompare(
-                    b.name,
-                    'pt-BR'
-                )
-        )
+
+
+    console.log(
+        '[GROUPS] Lista simplificada:'
+    )
+
+    console.table(
+        groups
+    )
 
 
     return groups
-
 }
 
 
 module.exports = {
-
     loadGroups
-
 }
